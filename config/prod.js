8246682a6da2ef242a6ed34934cef220174e3efa
NataliaJeszke <nataliajeszke8@gmail.com>
2023-05-07 20:50:40 +0200

    uninstalled chatbot-services library

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleProjectID: process.env.GOOGLE_PROJECT_ID,
+    dialogFlowSessionID: "react-bot-session",
+    dialogFlowSessionLanguageCode: "pl-PL",
+    userId: "100996922421257043917"
+  };
