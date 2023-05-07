3b713c2689a4f20f418c791a4d36f809ffc812cf
NataliaJeszke <nataliajeszke8@gmail.com>
2023-05-07 18:39:42 +0200

    working authorization, dialogflow response

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-const dialogflow = require("@google-cloud/dialogflow");
-require("dotenv").config();
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-const config = require("./config/keys");
-
-const sessionClient = new dialogflow.SessionsClient();
-const sessionPath = sessionClient.sessionPath(
-  config.googleProjectID,
-  config.dialogFlowSessionID
-);
-
-app.use(express.json());
+app.use(bodyParser.json());
 
 require("./routes/dialogFlowRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Serwer działa! Nasłuchuje na porcie ${PORT}`);
+});
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,37 +1,35 @@
 const dialogflow = require("@google-cloud/dialogflow");
-const express = require("express");
-
 const config = require("../config/keys");
+const { query } = require("express");
 
 const sessionClient = new dialogflow.SessionsClient();
-const sessionPath = sessionClient.sessionPath(
+
+const sessionPath = sessionClient.projectAgentSessionPath(
   config.googleProjectID,
   config.dialogFlowSessionID
 );
 
-const router = express.Router();
+module.exports = (app) => {
+  app.get("/", (req, res) => {
+    res.send({ Hello: "World" });
+  });
 
-router.get("/", (req, res) => {
-  res.send({ Hello: "World" });
-});
-
-router.post("/api/df_text_query", async (req, res) => {
-  const { query, languageCode } = req.body;
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        text: query,
-        languageCode,
+  app.post("/api/df_text_query", async (req, res) => {
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: req.body.text,
+          languageCode: config.dialogFlowSessionLanguageCode,
+        },
       },
-    },
-  };
-  let responses = await sessionClient.detectIntent(request);
-  res.send(responses[0].queryResult);
-});
+    };
 
-router.post("/api/df_event_query", (req, res) => {
-  res.send({ do: "event query" });
-});
+    let responses = await sessionClient.detectIntent(request);
+    res.send(responses[0].queryResult);
+  });
 
-module.exports = router;
+  app.post("/api/df_event_query", (req, res) => {
+    res.send({ do: "event query" });
+  });
+};
