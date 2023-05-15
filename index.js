55160f8cbd900fae4ff4ae8db01108ba44234130
NataliaJeszke <nataliajeszke8@gmail.com>
2023-05-15 13:23:36 +0200

    server connection cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require('cors');
 
 const config = require("./config/keys");
 const mongoose = require("mongoose");
